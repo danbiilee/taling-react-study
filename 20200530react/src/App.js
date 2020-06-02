e841523cdf3bf29bb18e54e717730e1c4369aebd
danbiilee <danbi.db@gmail.com>
2020-06-02 20:06:01 +0900

    Add 수정하기버튼/기능

--- 20200530react/src/App.js
+++ 20200530react/src/App.js
@@ -1,24 +1,17 @@
-// 1. 입력 인풋(제출)
-// 2. 고유 키
-// 3. 포커스 이동
-// 4. 삭제
-// 5. 수정 
-import React, { Component, createRef } from 'react';
+import React, { Component, createRef, useReducer } from 'react';
 
 class App extends Component {
   id = 1;
+
+  // 수정할 selectUser 속성 추가 
   state = {
     username: '',
     password: '',
     list: [],
-    editedId: '',
-    editedUsername: '',
-    editedPassword: '',
-    editedList: []
+    selectUser: {},
   };
 
   usernameInput = createRef();
-  editForm = createRef();
 
   handleChange = e => {
     const { name, value } = e.target;
@@ -32,14 +25,19 @@ class App extends Component {
     e.preventDefault();
     const { username, password, list } = this.state;
     
+    // isShow속성 추가 
     this.setState({
       username: '',
       password: '',
-      list: list.concat({
+      list: list.map(user => {
+        return {...user, isShow: false};
+      }).concat({
         username,
         password,
-        id: this.id 
-      })
+        id: this.id,
+        isShow: false 
+      }),
+      selectUser: {}
     });
 
     this.id++;
@@ -52,51 +50,63 @@ class App extends Component {
     });
   };
 
-  handleEditForm = id => {
+  handleShowForm = id => {
     const { list } = this.state;
-    const filterdList = list.filter(user => user.id === id);
 
     this.setState({
-      editedId: id,
-      editedUsername: filterdList[0].username,
-      editedPassword: filterdList[0].password
+      list: list.map(user => {
+        if(user.id === id) {
+          return {
+            ...user,
+            isShow: !user.isShow
+          };
+        } else {
+          return {
+            ...user,
+            isShow: false
+          };
+        }
+      }),
+      selectUser: {...list.filter(user => user.id === id)}[0]
     });
-
-    console.log(this.editForm);
-    this.editForm.current.style.display = 'block';
   };
 
   handleEditChange = e => {
     const { name, value } = e.target;
     this.setState({
-      [name]: value
+      selectUser: {
+        ...this.state.selectUser,
+        [name]: value
+      }
     });
   };
 
   handleEditSubmit = e => {
     e.preventDefault();
 
-    const { list, editedId, editedUsername, editedPassword } = this.state;
+    const { list, selectUser } = this.state;
     this.setState({
       list: list.map(user => {
-        if(user.id === editedId) {
-          return {
-            id: user.id,
-            username: editedUsername,
-            password: editedPassword
-          };
+        if(user.id === selectUser.id) {
+          return selectUser;
+          // return {
+          //   ...user,
+          //   username: selectUser.username,
+          //   password: selectUser.password,
+          //   isShow: false
+          // };
         } else return user;
       }),
-      editedId: '',
-      editedUsername: '',
-      editedPassword: ''
+      selectUser: {}
     });
-
-    this.editForm.current.style.display = 'none';
   };
 
   render() {
-    const { username, password, list, editedUsername, editedPassword } = this.state;
+    const { username, password, list, selectUser } = this.state;
+
+    function formStyle(bool) {
+      return bool ? {display: 'block'} : {display: 'none'};
+    }
 
     return (
       <div>
@@ -111,18 +121,12 @@ class App extends Component {
           list.map(user => {
             return (
               <li key={user.id}>
-                {user.username}의 비밀번호는 {user.password}입니다.
+                {user.username}의 비밀번호는 {user.password}입니다. 
                 <button type="button" onClick={() => this.handleDelete(user.id)}>삭제하기</button>
-                <button type="button" onClick={() => this.handleEditForm(user.id)}>수정하기</button>
-                {/* 
-                  1. 수정하기 버튼 클릭하면 수정폼 보여야함.
-                  2. 저장하기 버튼 클릭하면 기존 list배열 변경되고, 수정폼 다시 안보여야함.(리렌더)
-                  - username, password를 어디에서 관리할건지? 
-                  3. 다른 리스트의 수정하기 버튼 클릭하면 이전 수정폼 안보이고, 새로 클릭한 수정폼 보여야함. 
-                */}
-                <form style={{display: "none"}} ref={this.editForm} onSubmit={this.handleEditSubmit}>
-                  <input name="editedUsername" value={editedUsername} onChange={this.handleEditChange}></input>      
-                  <input name="editedPassword" value={editedPassword} onChange={this.handleEditChange}></input>      
+                <button type="button" onClick={() => this.handleShowForm(user.id)}>수정하기</button>
+                <form style={formStyle(user.isShow)} onSubmit={this.handleEditSubmit}>
+                  <input name="username" value={selectUser.username} onChange={this.handleEditChange}></input>      
+                  <input name="password" value={selectUser.password} onChange={this.handleEditChange}></input>      
                   <button type="submit">저장</button>
                 </form>
               </li>
