5185194d4f51572f70f4fa0edc5221eab1fdca3c
danbiilee <danbi.db@gmail.com>
2020-06-17 00:00:41 +0900

    Add homework4

---
+++ homework4/src/components/Main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="Main">
+        
+      </div>
+    );
+  }
+}
+
+export default Main;
