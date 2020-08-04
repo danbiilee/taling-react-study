f4f357ce4291a17314ce48f32a2d0c754f7849b2
danbiilee <danbi.db@gmail.com>
2020-08-04 20:56:10 +0900

    Change useState to useReducer

--- begin-react/src/hooks/useInputs.js
+++ begin-react/src/hooks/useInputs.js
@@ -1,15 +1,37 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useReducer } from 'react';
+
+function reducer(form, action) {
+  console.log(form, action);
+  switch(action.type) {
+    case 'CHANGE_INPUT':
+      return {
+        ...form, 
+        [action.name]: action.value,
+      };
+    case 'RESET_INPUTS':
+      return action.form;
+    default: 
+      return form;
+  }
+}
 
 function useInputs(initialForm) {
-  const [ form, setForm ] = useState(initialForm);
+  const [ form, dispatch ] = useReducer(reducer, initialForm);
   
   const onChange = useCallback(e => {
     const { name, value } = e.target;
-    setForm(form => ({...form, [name]: value}));
+    dispatch({
+      type: 'CHANGE_INPUT',
+      name,
+      value,
+    });
   }, []);
 
   const reset = useCallback(() => {
-    setForm(initialForm)
+    dispatch({ 
+      type: 'RESET_INPUTS', 
+      form: initialForm
+    });
   }, [initialForm]); 
 
   return [form, onChange, reset];
