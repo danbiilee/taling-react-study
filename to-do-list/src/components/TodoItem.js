cf1c1838047b5df237c39fd644ca3bcb12f220ad
danbiilee <danbi.db@gmail.com>
2020-08-13 11:52:35 +0900

    first commit

---
+++ to-do-list/src/components/TodoItem.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { MdDone, MdDelete } from 'react-icons/md';
+
+const Remove = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+  display: none;
+`;
+
+const TodoItemBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 12px 0;
+  &:hover {
+    ${Remove} {
+      display: initial;
+    }
+  }
+`;
+
+const CheckCircle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 32px;
+  hegiht: 32px;
+  margin-right: 20px;
+  border: 1px solid #ced4da;
+  border-radius: 16px;
+  font-size: 24px;
+  cursor: pointer;
+  ${(props) =>
+    props.done &&
+    css`
+      border: 1px solid #38d9a9;
+      color: #38d9a9;
+    `}
+`;
+
+const Text = styled.div`
+  flex: 1;
+  color: #495057;
+  font-size: 21px;
+  ${(props) =>
+    props.done &&
+    css`
+      color: #ced4da;
+    `}
+`;
+
+function TodoItem({ id, done, text }) {
+  return (
+    <TodoItemBlock>
+      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <Text done={done}>{text}</Text>
+      <Remove>
+        <MdDelete />
+      </Remove>
+    </TodoItemBlock>
+  );
+}
+
+export default TodoItem;
---
+++ to-do-list/src/components/TodoList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import TodoItem from './TodoItem';
+
+const TodoListBlock = styled.div`
+  overflow-y: auto;
+  flex: 1;
+  padding: 20px 32px 48px;
+`;
+function TodoList() {
+  return (
+    <TodoListBlock>
+      <TodoItem text="aaaaaaaa" done={true} />
+      <TodoItem text="bbbbbbbb" done={true} />
+      <TodoItem text="cccccccc" done={false} />
+      <TodoItem text="dddddddd" done={false} />
+    </TodoListBlock>
+  );
+}
+
+export default TodoList;
