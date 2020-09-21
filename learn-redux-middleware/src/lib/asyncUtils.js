1dffc400dfba4d3f53cfab0c497351d2abf343b3
danbiilee <danbi.db@gmail.com>
2020-09-21 20:50:04 +0900

    Refactor posts module and Add to root reducer

--- learn-redux-middleware/src/lib/asyncUtils.js
+++ learn-redux-middleware/src/lib/asyncUtils.js
@@ -43,3 +43,30 @@ export const reducerUtils = {
     error,
   }),
 };
+
+// 비동기 관련 액션들을 처리하는 리듀서를 만들어주는 함수
+// type =  action.type, key = state.key(예: posts, post)
+export const handleAsyncActions = (type, key) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  return (state, action) => {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: reducerUtils.loading,
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: reducerUtils.success(action.payload),
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: reducerUtils.error(action.payload),
+        };
+      default:
+        return state;
+    }
+  };
+};
--- learn-redux-middleware/src/modules/posts.js
+++ learn-redux-middleware/src/modules/posts.js
@@ -1,4 +1,9 @@
 import * as postsAPI from '../api/posts';
+import {
+  createPromiseThunk,
+  reducerUtils,
+  handleAsyncActions,
+} from '../lib/asyncUtils';
 
 /* 액션 타입 */
 // 포스트 여러 개 조회
@@ -12,95 +17,28 @@ const GET_POST_ERROR = 'GET_POST_ERROR';
 
 // thunk를 사용할 때 꼭 모든 액션들에 대해 액션 생성함수를 만들지 않아도 됨
 // thunk 함수에서 바로 액션 객체를 만들어도 됨
-export const getPosts = () => async dispatch => {
-  dispatch({ type: GET_POSTS }); // 요청 시작됨
-  try {
-    const posts = await postsAPI.getPosts(); // API 호출
-    dispatch({ type: GET_POSTS_SUCCESS, posts }); // 성공
-  } catch (e) {
-    dispatch({ type: GET_POSTS_ERROR, error: e }); // 실패
-  }
-};
-
-export const getPost = id => async dispatch => {
-  dispatch({ type: GET_POST });
-  try {
-    const post = await postsAPI.getPostById(id);
-    dispatch({ type: GET_POST_SUCCESS, post });
-  } catch (e) {
-    dispatch({ type: GET_POST_ERROR, error: e });
-  }
-};
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
 
 const initialState = {
-  posts: {
-    loading: false,
-    data: null,
-    error: null,
-  },
-  post: {
-    loading: false,
-    data: null,
-    error: null,
-  },
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial(),
 };
 
 export default function posts(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: null,
-          error: null,
-        },
-      };
     case GET_POSTS_SUCCESS:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: action.posts,
-          error: null,
-        },
-      };
     case GET_POSTS_ERROR:
-      return {
-        ...state,
-        posts: {
-          loading: true,
-          data: null,
-          error: action.error,
-        },
-      };
+      //return handleAsyncActions(GET_POSTS, 'posts')(state, action);
+      const postsReducer = handleAsyncActions(GET_POSTS, 'posts');
+      return postsReducer(state, action);
     case GET_POST:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: null,
-          error: null,
-        },
-      };
     case GET_POST_SUCCESS:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: action.post,
-          error: null,
-        },
-      };
     case GET_POST_ERROR:
-      return {
-        ...state,
-        post: {
-          loading: true,
-          data: null,
-          error: action.error,
-        },
-      };
+      //return handleAsyncActions(GET_POST, 'post')(state, action);
+      const postReducer = handleAsyncActions(GET_POST, 'post');
+      return postReducer(state, action);
     default:
       return state;
   }
